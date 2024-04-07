7e8d6296c3dbe4aa56b32a813ef4caf6a22efa06
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2024-04-07 09:34:07 +0100

    MEOW-01: move logic to  library crate, expose the  entrypoint in main

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 
+async fn health_check(_: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
