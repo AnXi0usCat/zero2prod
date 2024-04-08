2958091956fc9731e972fd2cb873d5cc89e2ea77
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2024-04-08 20:51:39 +0100

    MEOW-01: dummy implementation of the subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,18 @@ async fn health_check(_: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(_: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/sunscribe", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
