6be38c837346b49181547b0938089edb911872cf
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2024-04-18 21:49:09 +0100

    MEOW-01: fix ip address error

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::{configuration::get_configuration, startup::run};
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to  read configuration.");
-    let address = format!("127.0.0.1.1:{}", configuration.application_port);
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to port.");
     run(listener)?.await
 }
