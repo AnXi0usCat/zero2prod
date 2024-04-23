f19409fe97585979b6c11afb3c7c27883f11f17e
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2024-04-23 21:52:34 +0100

    MEOW-01: formatting

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use sqlx::PgConnection;
 use uuid::Uuid;
-use chrono::Utc;
-
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -11,11 +10,21 @@ pub struct FormData {
     email: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgConnection>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    connection: web::Data<PgConnection>,
+) -> HttpResponse {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
-        "#, Uuid::new_v4(), form.email, form.name, Utc::now()).execute(connection.get_ref()).await;
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
