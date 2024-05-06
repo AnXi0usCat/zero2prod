3f2b36bb237c401f2d010e3110a2173c985aea06
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2024-05-06 16:22:32 +0100

    MEOW-01: formatting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,8 @@
+use once_cell::sync::Lazy;
 use reqwest::StatusCode;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
-use once_cell::sync::Lazy;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::get_subscriber;
